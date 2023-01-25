91641c2109a8bf1b985d48942e2cbb60cbf44b1e
Matveev Andrey <circlsun@gmail.com>
2023-01-25 10:33:52 +0300

    Add gitignore

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
+import os
 import requests
 
-url = "https://tululu.org/txt.php?id=32168"
 
-response = requests.get(url)
-response.raise_for_status()
+i = 32169
+url = f'https://tululu.org/txt.php?id={i}'
 
-filename = '«Пески Марса»'
-with open(filename, 'wb') as file:
-    file.write(response.content)
+
+def save_book(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
+filename = f'id{i}'
+os.makedirs('books', exist_ok=True)
+path = f'{os.getcwd()}/books/{filename}'
+
+
+for i in range(32169, 32179):
+    save_book(url, path)

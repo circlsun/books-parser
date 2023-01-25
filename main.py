f8468e65f6e75c2353fd93f87e2e6d12bf1c83b7
Matveev Andrey <circlsun@gmail.com>
2023-01-25 16:17:59 +0300

    Add parse for 10 books

--- main.py
+++ main.py
@@ -2,21 +2,28 @@ import os
 import requests
 
 
-i = 32169
-url = f'https://tululu.org/txt.php?id={i}'
-
-
 def save_book(url, path):
-    response = requests.get(url)
+    response = requests.get(url, allow_redirects=True)
     response.raise_for_status()
     with open(path, 'wb') as file:
         file.write(response.content)
+    check_for_redirect(response)
+    return response.url
 
 
-filename = f'id{i}'
+def check_for_redirect(response):
+    if response.url == 'https://tululu.org/':
+        raise requests.exceptions.HTTPError
+
+
 os.makedirs('books', exist_ok=True)
-path = f'{os.getcwd()}/books/{filename}'
 
+id_start = 200
+for i in range(id_start, id_start + 10):
+    path = f'{os.getcwd()}/books/id{i}.txt'
+    url = f'https://tululu.org/txt.php?id={i}'
 
-for i in range(32169, 32179):
-    save_book(url, path)
+    try:
+        save_book(url, path)
+    except requests.exceptions.HTTPError:
+        os.remove(f'{os.getcwd()}/books/id{i}.txt')

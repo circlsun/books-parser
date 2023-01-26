23840bb6a0fe9d34f63fd1e3b22fd2373b84c16d
circlsun <circlsun@gmail.com>
2023-01-26 17:07:22 +0300

    Download comments

--- bs4_tutorial.py
+++ bs4_tutorial.py
@@ -1,8 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
 
-url = 'https://tululu.org/b239/'
+url = 'https://tululu.org/b5/'
 response = requests.get(url)
 response.raise_for_status()
 
@@ -11,8 +12,13 @@ soup = BeautifulSoup(response.text, 'lxml')
 title_teg = soup.find('h1').text.split('::')
 title = title_teg[0].strip()
 author = title_teg[1].strip()
+comments = soup.find_all(class_='texts')
+for comment in comments:
+    print('Комменты:', comment.select('span'))
+
 img = soup.find(class_='bookimage').find('a').find('img')['src']
+url_img = urljoin('https://tululu.org/', img)
 
 print('Автор:', author)
 print('Заголовок:', title)
-print('Обложка:', img)
+print('Обложка:', url_img)

078c7b292abb2c2ab2a123b63c03473d2b40b763
circlsun <circlsun@gmail.com>
2023-01-26 15:14:27 +0300

    Download book covers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from pathvalidate import sanitize_filename
+from urllib.parse import urljoin, urlsplit
 from bs4 import BeautifulSoup
 
 
@@ -21,6 +22,7 @@ def download_txt(url, filename, folder='books'):
 
     name = f"{sanitize_filename(filename)}"
     path = f'{os.getcwd()}/{folder}/{name}.txt'
+
     with open(path, 'wb') as file:
         file.write(response.content)
 
@@ -28,12 +30,29 @@ def download_txt(url, filename, folder='books'):
     return os.path.join(folder, name)
 
 
+def download_image(url, name, folder='images'):
+    """Функция для скачивания картинок"""
+    response = requests.get(url)
+    response.raise_for_status()
+
+    path = f'{os.getcwd()}/{folder}/{name}'
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+    return os.path.join(folder, name)
+
+
 def get_filename(url):
     response = requests.get(url)
     response.raise_for_status()
+
     soup = BeautifulSoup(response.text, 'lxml')
     title_teg = soup.find('h1').text.split('::')
-    return title_teg[0].strip()
+    if response.url != 'https://tululu.org/':
+        img = soup.find(class_='bookimage').find('a').find('img')['src']
+    else:
+        img = 'images/nopic.gif'
+    return title_teg[0].strip(), urljoin('https://tululu.org/', img)
 
 
 def check_for_redirect(response):
@@ -43,13 +62,19 @@ def check_for_redirect(response):
 
 def main():
     os.makedirs('books', exist_ok=True)
-    id_start = 1
+    os.makedirs('images', exist_ok=True)
+    id_start = 300
     for i in range(id_start, id_start + 10):
         url = f'https://tululu.org/txt.php?id={i}'
         url_title = f'https://tululu.org/b{i}/'
-        filename = f"{i}. {get_filename(url_title)}"
+
+        title, img_url = get_filename(url_title)
+        imgname = urlsplit(img_url).path.split('/')[2]
+        filename = f"{i}. {title}"
+
         try:
-            download_txt(url, filename, folder='books')
+            download_txt(url, filename)
+            download_image(img_url, imgname)
         except requests.exceptions.HTTPError:
             os.remove(f'{os.getcwd()}/books/{filename}.txt')
 

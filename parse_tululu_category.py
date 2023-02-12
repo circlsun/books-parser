835af362c5d30f86360871035b0b406423fe3f6e
Matveev Andrey <circlsun@gmail.com>
2023-02-12 14:27:39 +0300

    +PEP8

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,10 +2,11 @@ import os
 import time
 import json
 import textwrap
-import requests
 import argparse
-from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlsplit
+import requests
+from bs4 import BeautifulSoup
+
 from pathvalidate import sanitize_filename
 
 
@@ -178,7 +179,9 @@ def main():
         with open(
             f'{json_folder}/{"books_description.json"}', 'w', encoding='utf8'
         ) as json_file:
-            json_file.write(json.dumps(books_description, ensure_ascii=False, indent=2))
+            json_file.write(
+                json.dumps(books_description, ensure_ascii=False, indent=2)
+            )
 
 
 if __name__ == "__main__":
